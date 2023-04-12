4ec3f414a5e49c76117e6142500920a21ce926d5
sebastian caro <seba-handball@outlook.com>
2023-04-12 20:10:47 -0300

    Primer preentrega

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+    <nav>
+        <img src="./" alt="" />
+        <h3>
+            Odil Comidas Caseras 
+        </h3>
+        <div>
+            <button>Panes</button>
+            <button>Postres</button>
+            <button>Conocenos</button>
+            <button> Nuestros Productos</button>
+        </div>
+    </nav>
+    )
+}
+
+export default NavBar
